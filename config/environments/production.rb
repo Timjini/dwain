fe8597c2f535732ece9ea214acced5586afacdfc
timjini <hatim.jini@gmail.com>
2023-02-06 14:09:55 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'chambersforsport.com',
     address: 'smtp.sendgrid.net',
     port: 587,
