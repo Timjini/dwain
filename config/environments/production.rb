f635e4b2b265844bc930e01a0aed61be0d5a5a6c
timjini <hatim.jini@gmail.com>
2023-02-06 14:00:27 +0300

    new env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,8 @@ Rails.application.configure do
   # }
 
   config.action_mailer.smtp_settings = {
-    user_name: 'apikey',
-    password: Rails.application.credentials.dig(:sendgrid , :api_key),
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
     domain: 'chambersforsport.com',
     address: 'smtp.sendgrid.net',
     port: 587,
