5968ba3c1dfce516674f60ba696644a1d46aa094
Timjini <hatim.jini@gmail.com>
2023-02-16 23:37:01 +0300

    changes to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  root 'pages#comingsoon'
+  # root 'pages#comingsoon'
+  root 'pages#index'
 
   #static pages
-  # get 'contact' => 'pages#contact'
-  # get 'talks' => 'pages#talks'
-  # get 'camps' => 'pages#camps'
-  # get 'dcpa' => 'pages#dcpa'
-  # get 'groups' => 'pages#groups'
-  # get 'cfs' => 'pages#cfs'
-  get 'thank-you' => 'pages#index'
+  get 'contact' => 'pages#contact'
+  get 'talks' => 'pages#talks'
+  get 'camps' => 'pages#camps'
+  get 'dcpa' => 'pages#dcpa'
+  get 'groups' => 'pages#groups'
+  get 'cfs' => 'pages#cfs'
+  get 'thank-you' => 'pages#thank_you'
 
   #contact form
   resources :contacts, only: [:new, :create]
