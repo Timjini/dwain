51aed25f7769acae46f0b10cb7a40df566c92e38
Timjini <hatim.jini@gmail.com>
2023-03-04 22:45:14 +0300

    dwain page and new navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,10 @@ Rails.application.routes.draw do
   get 'camps' => 'pages#camps'
   get 'dcpa' => 'pages#dcpa'
   get 'groups' => 'pages#groups'
-  get 'cfs' => 'pages#cfs'
   get 'thank-you' => 'pages#thank_you'
   get 'login' => 'pages#login'
   get 'dwain' => 'pages#dwain'
-  get 'faq'=> 'pages#faq'
+  get 'cfs'=> 'pages#faq'
   get 'form' => 'pages#form'
 
   #contact form
