94f77e2a6fb9e459e34e127b3f3e2bc63f9543f1
Timjini <hatim.jini@gmail.com>
2023-05-05 10:18:41 +0300

    cors.rb update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,8 @@
-# config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3000'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-    end
-
-    allow do
-        origins 'https://www.chambersforsport.com/'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: true
-    end
+  allow do
+    origins 'https://www.chambersforsport.com'
+    resource '/contacts',
+      :headers => :any,
+      :methods => [:post]
   end
+end
