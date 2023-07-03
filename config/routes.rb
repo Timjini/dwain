3500e8f506ccb345424d370b6f06023b09997c31
Timjini <hatim.jini@gmail.com>
2023-07-04 01:02:50 +0300

    coach page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :edit, :update]
   resources :coaches, only: [:index, :show, :edit, :update]
-  resources :teams, only: [:index, :show, :edit, :update, :create, :new]
+  # resources :teams, only: [:index, :show, :edit, :update, :create, :new]
   
   # Root path for coaches
   authenticated :coach do
