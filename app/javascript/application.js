c2f4c15f91a95e846be21432cd02d8ccd8f75ecf
Timjini <hatim.jini@gmail.com>
2023-08-10 16:57:09 +0300

    update js controller path

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
-import "controllers/hello_controller.js"
-import "controllers/training_table_controller.js"
+import "./controllers/hello_controller.js"
+import "./controllers/training_table_controller.js"
 
 Turbo.start()
 
